60d1631fa4f3e65f644e9b9370d3f2fcc0810664
slimerence <39113678+slimerence@users.noreply.github.com>
2019-04-08 12:53:09 +1000

    Update work.php

--- wp-content/themes/eduonline/learningonline/single-course/tabs/work.php
+++ wp-content/themes/eduonline/learningonline/single-course/tabs/work.php
@@ -6,8 +6,10 @@
  * Time: 11:44
  */
 
-$content=apply_filters('the_content', get_post_field('post_content', 1238));
+$post = get_post(1238);
+
+$content = $post->post_content;
 
 
-echo $content;
 
+var_dump($content);
