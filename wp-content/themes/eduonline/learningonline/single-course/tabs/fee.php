b9c0d666ebcab3cf2400612fc71064243426b712
slimerence <39113678+slimerence@users.noreply.github.com>
2019-04-28 22:20:09 +1000

    update

--- wp-content/themes/eduonline/learningonline/single-course/tabs/fee.php
+++ wp-content/themes/eduonline/learningonline/single-course/tabs/fee.php
@@ -6,7 +6,12 @@
  * Time: 11:44
  */
 
-$post = get_post(1386);
+$course = learning_online_get_the_course();
 
+if($course->id == 665){
+    $post = get_post(1386);
+}else{
+    $post = get_post(1386);
+}
 $content = $post->post_content;
 echo do_shortcode($content);
--- wp-content/themes/eduonline/learningonline/single-course/tabs/intake.php
+++ wp-content/themes/eduonline/learningonline/single-course/tabs/intake.php
@@ -6,4 +6,11 @@
  * Time: 20:08
  */
 $course = learning_online_get_the_course();
-var_dump($course);
+
+if($course->id == 665){
+    $post = get_post(1693);
+}else{
+    $post = get_post(1693);
+}
+$content = $post->post_content;
+echo do_shortcode($content);
