c4e4cb27dfc7394eb2015aaf056b1439c8c99afd
slimerence <39113678+slimerence@users.noreply.github.com>
2019-04-28 21:58:55 +1000

    update

---
+++ wp-content/themes/eduonline/learningonline/single-course/tabs/entry.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Claude
+ * Date: 2019/4/26
+ * Time: 20:08
+ */
---
+++ wp-content/themes/eduonline/learningonline/single-course/tabs/intake.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Claude
+ * Date: 2019/4/26
+ * Time: 20:08
+ */
+$course = learning_online_get_the_course();
+var_dump($course);
